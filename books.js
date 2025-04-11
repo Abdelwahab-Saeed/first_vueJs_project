75421159d805366f2140180c259f3bc8c051899d
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-04-11 17:25:52 +0200

    Add all files with the code

---
+++ books.js
@@ -0,0 +1,134 @@
+books = [
+    {
+        id:1,
+        name:'The-Elements-of-the-Crown',
+        price:1000,
+        instock:100,
+        ISBN:'123',
+        pages:100,
+        author:'Ali',
+        category:'Fantasy',
+        image:"https://kaylmoody.com/wp-content/uploads/2021/09/The-Elements-of-the-Crown-NEW-web-cover-600x900.jpg"
+    },
+    {
+        id:2,
+        name:'Lemon 8',
+        price:2000,
+        instock:0,
+        ISBN:'456',
+        pages:150,
+        author:'Mohamed',
+        category:'Action',
+        image:"https://api16-normal-useast1a.lemon8-app.com/seo/image?item_id=7390048756907491845&index=0&sign=1a767d1311eae992bcf1fb78d555696c"
+    },
+    {
+        id:3,
+        name:'The widow',
+        price:3000,
+        instock:100,
+        ISBN:'789',
+        pages:45,
+        author:'Mostafa',
+        category:'Action',        
+        image:"https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.penguinrandomhouse.com/cover/d/9780385548984"
+    },
+    {
+        id:4,
+        name:'The poppy war',
+        price:4000,
+        instock:50,
+        ISBN:'987',
+        pages:50,
+        author:'Hassan',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/16/81laymbisll_custom-3672cf847e374ab050b21d1ab215772a6ddc14fc.jpeg?s=1100&c=50&f=jpeg"
+    },
+    {
+        id:5,
+        name:'The calculating stars',
+        price:5000,
+        instock:30,
+        ISBN:'654',
+        pages:30,
+        author:'Ahmed',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/13/711gd93ifkl_custom-80dd66e555a05cf605f043ca0095760a75d41be4.jpeg?s=1100&c=50&f=jpeg"
+    },
+    {
+        id:6,
+        name:'The-Elements-of-the-Crown',
+        price:1000,
+        instock:45,
+        ISBN:'321',
+        pages:100,
+        author:'Khaled',
+        category:'Fantasy',
+        image:"https://kaylmoody.com/wp-content/uploads/2021/09/The-Elements-of-the-Crown-NEW-web-cover-600x900.jpg"
+    },
+    {
+        id:7,
+        name:'Lemon 8',
+        price:2000,
+        instock:30,
+        ISBN:'257',
+        pages:150,
+        author:'Author Two',
+        category:'Category Two',
+        image:"https://api16-normal-useast1a.lemon8-app.com/seo/image?item_id=7390048756907491845&index=0&sign=1a767d1311eae992bcf1fb78d555696c"
+    },
+    {
+        id:8,
+        name:'The widow',
+        price:3000,
+        instock:100,
+        ISBN:'698',
+        pages:45,
+        author:'Author Three',
+        category:'Category Three',        
+        image:"https://res.cloudinary.com/itimages/image/fetch/c_scale,w_400,q_auto:eco,f_auto/https://images.penguinrandomhouse.com/cover/d/9780385548984"
+    },
+    {
+        id:9,
+        name:'The poppy war',
+        price:4000,
+        instock:100,
+        ISBN:'167',
+        pages:50,
+        author:'Author Four',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/16/81laymbisll_custom-3672cf847e374ab050b21d1ab215772a6ddc14fc.jpeg?s=1100&c=50&f=jpeg"
+    },
+    {
+        id:10,
+        name:'The calculating stars',
+        price:5000,
+        instock:100,
+        ISBN:'198',
+        pages:30,
+        author:'Author Five',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/13/711gd93ifkl_custom-80dd66e555a05cf605f043ca0095760a75d41be4.jpeg?s=1100&c=50&f=jpeg"
+    },
+    {
+        id:11,
+        name:'The poppy war',
+        price:4000,
+        instock:100,
+        ISBN:'129',
+        pages:50,
+        author:'Author Four',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/16/81laymbisll_custom-3672cf847e374ab050b21d1ab215772a6ddc14fc.jpeg?s=1100&c=50&f=jpeg"
+    },
+    {
+        id:12,
+        name:'The calculating stars',
+        price:5000,
+        instock:100,
+        ISBN:'762',
+        pages:30,
+        author:'Author Five',
+        category:'Science fiction',
+        image:"https://media.npr.org/assets/img/2021/08/13/711gd93ifkl_custom-80dd66e555a05cf605f043ca0095760a75d41be4.jpeg?s=1100&c=50&f=jpeg"
+    }
+]
